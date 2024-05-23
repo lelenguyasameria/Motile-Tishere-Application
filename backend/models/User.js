7a5239f4b0bcae68822920e6b11d57e8d10c0923
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-23 15:13:15 +0300

    Handles user data configuration

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+	firstName: {
+		type: String,
+		required: true,
+		min: 2,
+		max: 50,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		min: 2,
+		max: 50,
+	},
+	email: {
+		type: String,
+		required: true,
+		max: 50,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		min: 8,
+	},
+	picturePath: {
+		type: String,
+		default: "",
+	},
+	connections: {
+		type: Array,
+		default: [],
+	},
+	location: String,
+	occupation: String,
+	viewedProfile: Number,
+	impressions: Number,
+}, { timestamps: true });
+
+const User = mongoose.model("User", UserSchema);
+export default User;
