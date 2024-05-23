6670f8660fbb0fc8304538bc4753c61c5e642ee0
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-23 16:25:03 +0300

    Implemented add and remove connection from users

---
+++ backend/controllers/users.js
@@ -0,0 +1,63 @@
+import User from "../models/User";
+
+/* READ */
+export const getUser = async (req, res) => {
+  try {
+		const { id } = req.params;
+		const useer = await User.findById(id);
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+}
+
+export const getUserConnections = async(req, res) => {
+	try {
+			const { id } = req.params;
+	const user = await User.findById(id);
+
+	const connections = await Promise.all(
+		user.friends.map((id) => User.findById(id))
+	);
+	const formattedConnections = friends.map(
+		({ _id, firstName, lastName, occupation, location, picturePath }) => {
+			return { _id, firstName, lastName, occupation, location, picturePath };
+		}
+	);
+	res.status(200).json(formattedConnections);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
+
+/* UPDATE */
+export const addRemoveConnection = async (req, res) => {
+	try {
+		const { id, connectionId } = req.params;
+		const user = await User.findById(id);
+		const connection = await User.findById(connectionId);
+
+		if (user.connections.includes(connectionId)) {
+			user.connections = user.connections.filter((id) => id !== connectionId);
+			connection.connections = connection.connections.filter((id) => id !== Id);
+		} else {
+			user.connections.push(friendId);
+			connection.connections(id);
+		}
+		await user.save();
+		await connection.save();
+
+		const connections = await Promise.all(
+			user.friends.map((id) => User.findById(id))
+		);
+		const formattedConnections = friends.map(
+			({ _id, firstName, lastName, occupation, location, picturePath }) => {
+				return { _id, firstName, lastName, occupation, location, picturePath };
+			}
+		);
+
+		res.status(200).json(formattedConnections);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+}
