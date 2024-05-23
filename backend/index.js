ae3de8c1c479f9f14d284d28a88b4f2a254b75a7
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-23 14:51:52 +0300

    Added configuration for the backend

--- backend/index.js
+++ backend/index.js
@@ -32,4 +32,14 @@ const storage = multer.diskStorage({
 		cb(null, file.originalname);
 	}
 });
-const upload = multer({ storage});
+const upload = multer({ storage });
+
+/* SETUP FOR MONNGOOSE */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => {
+	app.listen(PORT, () =>
+	console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
