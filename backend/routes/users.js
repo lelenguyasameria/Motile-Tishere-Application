90c9870d26cada8c983c38539a007a1550455a02
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-23 16:01:40 +0300

    Added routes for user

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  getUser,
+	getUserConnections,
+	addRemoveConnection,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get(":id/connections", verifyToken, getUserConnections);
+
+/* UPDATE */
+router.patch("/:id/:connectionId", verifyToken, addRemoveConnection);
+
+export default router;
