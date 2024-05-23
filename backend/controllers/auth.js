e647d6ae33af1c60ce2558d0f7db02d3739af9e0
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-23 15:25:05 +0300

    Implemented registration of user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,39 @@
 import brycpt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js'
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+		const {
+			firstName,
+			lastName,
+			email,
+			password,
+			picturePath,
+			connections,
+			location,
+			occupation
+		} = req.body;
+
+		const salt = await brycpt.genSalt();
+		const passwordHash = await brycpt.hash(password, salt);
+
+		const newUser = newUser({
+			firstName,
+			lastName,
+			email,
+			password: passwordHash,
+			picturePath,
+			connections,
+			location,
+			occupation,
+			viewedProfile: Math.floor(Math.random() * 1000),
+			impressions: Math.floor(Math.random() *10000)
+		});
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
