d1e054e3e1b87f2cae8ce3de001a9385ebbbd63a
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-24 10:28:27 +0300

    Handles Post for a user

---
+++ backend/models/Post.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+		userId: {
+			type: String,
+			requiered: true,
+		},
+		firstName: {
+			type: String,
+			requiered: true,
+		},
+		lastName: {
+			type: String,
+			requiered: true,
+		},
+		location: String,
+		description: String,
+		picturePath: String,
+		userPicturePath: String,
+		likes: {
+			type: Map,
+			of: Boolean,
+		},
+		comments: {
+			types: Array,
+			default: []
+		}
+	},
+	{ timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
