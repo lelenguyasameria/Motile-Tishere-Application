6bb1ee7d66eac962263edcfb3164e8cae3ead6b3
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-27 14:44:41 +0300

    Added google font for styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import { AuthProvider } from "./context/AuthContext";
-
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import Header from "./components/Header";
-
-import PrivateRoute from "./utils/PrivateRoute";
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <AuthProvider>
-        <Header />
-        <Routes>
-          <Route path="/" element={
-          <PrivateRoute>
-            <HomePage/></PrivateRoute>} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-        </AuthProvider>
-      </Router>
+    <div className="app">
+
     </div>
   );
 }
