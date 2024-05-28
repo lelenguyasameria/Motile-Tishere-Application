7b0be50b36cfd7b9c446274ee40994e70933c260
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-28 16:20:45 +0300

    Hnadles navbar for all devices

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Motile
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
