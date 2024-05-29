b99aaeb2f45a05d2217ee0cb452760d3458fe51a
Yang <yangmunene@gmail.com>
2024-05-29 13:27:06 +0300

    Adjusted the path to the corresponding components

--- frontend/src/scenes/widgets/MyPostWidget.jsx
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -17,13 +17,13 @@ import {
     IconButton,
     useMediaQuery,
   } from "@mui/material";
-  import FlexBetween from "components/FlexBetween";
+  import FlexBetween from "../../components/FlexBetween";
   import Dropzone from "react-dropzone";
   import UserImage from "components/UserImage";
   import WidgetWrapper from "components/WidgetWrapper";
   import { useState } from "react";
   import { useDispatch, useSelector } from "react-redux";
-  import { setPosts } from "state";
+  import { setPosts } from "../../state";
   
   const MyPostWidget = ({ picturePath }) => {
     const dispatch = useDispatch();
